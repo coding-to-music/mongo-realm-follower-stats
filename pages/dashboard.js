f738c0eb1ce6b6f97ca070454a0623753ca5812f
geekysrm <soumyacool2012@gmail.com>
2022-01-12 10:44:43 +0530

    update logic of settings page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,61 +2,64 @@ import Head from "next/head";
 import { useState, useEffect } from "react";
 import * as Realm from "realm-web";
 import { useRouter } from "next/router";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Link from "next/link";
 
 export default function ProtectedDashboardPage() {
+  const { user, error, isLoading } = useUser();
   const [socialMediaNames, setSocialMediaNames] = useState([]);
   const [socialData, setSocialData] = useState(undefined);
 
-  const currentUserEmail = "newnewnew@gmail.com";
+  // useEffect(() => {
+  //   async function fn() {
+  //     const REALM_APP_ID = "followers_tracker-vlmoo";
+  //     const app = new Realm.App({ id: REALM_APP_ID });
+  //     const credentials = Realm.Credentials.anonymous();
+  //     try {
+  //       const mongoUser = await app.logIn(credentials);
+  //       const userPresent = await mongoUser.functions.findUser(user.email);
+  //       if (userPresent) {
+  //         setSocialMediaNames(userPresent.socials);
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
-  useEffect(() => {
-    async function fn() {
-      const REALM_APP_ID = "followers_tracker-vlmoo";
-      const app = new Realm.App({ id: REALM_APP_ID });
-      const credentials = Realm.Credentials.anonymous();
-      try {
-        const user = await app.logIn(credentials);
-        const userPresent = await user.functions.findUser(currentUserEmail);
-        if (userPresent) {
-          setSocialMediaNames(userPresent.socials);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  //   fn();
+  // }, []);
 
-    fn();
-  }, []);
+  // useEffect(() => {
+  //   async function fn() {
+  //     const REALM_APP_ID = "followers_tracker-vlmoo";
+  //     const app = new Realm.App({ id: REALM_APP_ID });
+  //     const credentials = Realm.Credentials.anonymous();
+  //     try {
+  //       let finalData = {};
+  //       const mongoUser = await app.logIn(credentials);
+  //       for (let i = 0; i < socialMediaNames.length; i++) {
+  //         const data = await mongoUser.functions.getFollowers({
+  //           email: user.email,
+  //           social: socialMediaNames[i],
+  //         });
+  //         console.log(data);
 
-  useEffect(() => {
-    async function fn() {
-      const REALM_APP_ID = "followers_tracker-vlmoo";
-      const app = new Realm.App({ id: REALM_APP_ID });
-      const credentials = Realm.Credentials.anonymous();
-      try {
-        let finalData = {};
-        const user = await app.logIn(credentials);
-        for (let i = 0; i < socialMediaNames.length; i++) {
-          const data = await user.functions.getFollowers({
-            email: currentUserEmail,
-            social: socialMediaNames[i],
-          });
-          console.log(data);
+  //         finalData[socialMediaNames[i]] = {
+  //           username: data.username,
+  //           followers: data.followers,
+  //         };
+  //       }
+  //       setSocialData(finalData);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
-          finalData[socialMediaNames[i]] = {
-            username: data.username,
-            followers: data.followers,
-          };
-        }
-        setSocialData(finalData);
-      } catch (error) {
-        console.error(error);
-      }
-    }
+  //   fn();
+  // }, [socialMediaNames]);
 
-    fn();
-  }, [socialMediaNames]);
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -66,7 +69,7 @@ export default function ProtectedDashboardPage() {
       </Head>
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        Welcome to Dashboard, <b>{currentUserEmail}</b>
+        Welcome to Dashboard, <b>{user.name}</b>
         {socialMediaNames?.length > 0 &&
           socialMediaNames?.map((socialMediaName) => (
             <div>{JSON.stringify(socialMediaName)}</div>
@@ -74,6 +77,12 @@ export default function ProtectedDashboardPage() {
         <br />
         <br />
         {JSON.stringify(socialData)}
+        <div>
+          GO to{" "}
+          <Link href="/settings">
+            <a>Settings</a>
+          </Link>
+        </div>
       </main>
     </div>
   );
--- pages/settings.js
+++ pages/settings.js
@@ -1,10 +1,68 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 import * as Realm from "realm-web";
-import { useRouter } from "next/router";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+const supportedSocialMedia = ["twitter", "github"];
 
 export default function ProtectedSettingsPage() {
+  const { user, error, isLoading } = useUser();
+
+  const [socialMediaNames, setSocialMediaNames] = useState([]);
+  const [socialData, setSocialData] = useState(undefined);
+
+  useEffect(() => {
+    async function fn() {
+      const REALM_APP_ID = "followers_tracker-vlmoo";
+      const app = new Realm.App({ id: REALM_APP_ID });
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        const mongoUser = await app.logIn(credentials);
+        const userPresent = await mongoUser.functions.findUser(user.email);
+        if (userPresent) {
+          setSocialMediaNames(userPresent.socials);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fn();
+  }, [user]);
+
+  useEffect(() => {
+    async function fn() {
+      const REALM_APP_ID = "followers_tracker-vlmoo";
+      const app = new Realm.App({ id: REALM_APP_ID });
+      const credentials = Realm.Credentials.anonymous();
+      try {
+        let finalData = {};
+        const mongoUser = await app.logIn(credentials);
+        for (let i = 0; i < socialMediaNames.length; i++) {
+          const data = await mongoUser.functions.getFollowers({
+            email: user.email,
+            social: socialMediaNames[i],
+          });
+
+          finalData[socialMediaNames[i]] = {
+            username: data.username,
+            followers: data.followers,
+          };
+        }
+
+        setSocialData(finalData);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fn();
+  }, [user, socialMediaNames]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+  console.log(socialMediaNames);
+  console.log(socialData);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -14,6 +72,30 @@ export default function ProtectedSettingsPage() {
 
       <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
         <h2>Settings Page</h2>
+        <div>Hey {user.name}, here are your enabled social media:</div>
+        <div>
+          {supportedSocialMedia.map((sm) => {
+            const socialMediaPresent = socialMediaNames.includes(sm);
+            let smUsername = "";
+            if (socialData && socialData[sm]) {
+              smUsername = socialData[sm]?.username;
+            }
+            return (
+              <div>
+                {sm} :{" "}
+                <input
+                  className="border-2 border-red-200"
+                  defaultValue={
+                    socialMediaPresent ? smUsername || "Loading..." : ""
+                  }
+                  readOnly={!!smUsername}
+                />{" "}
+                <button>X</button>
+              </div>
+            );
+          })}
+        </div>
+        <button onClick={() => {}}>Save</button>
       </main>
     </div>
   );
